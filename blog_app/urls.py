35ce01e6872b595ea7cea6dcb234220dcd4a2743
Obiora Amobi <obiora.amobi@gmail.com>
2016-04-06 16:38:55 +0100

    URLs Changed

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^blog/$', views.post_list),
-    url(r'^blog/(?P<id>\d+)/$', views.post_detail),
+    url(r'^$', views.post_list),
+    url(r'^(?P<id>\d+)/$', views.post_detail),
     url(r'^post/new/$', views.new_post, name='new_post'),
     # url(r'^post/edit/$', views.edit_post, name='edit_post'),
 ]
